75ee88901f8e23ece5b56ee661ffa82231dade23
Yukai Huang <yukaihuangtw@gmail.com>
2019-09-21 16:42:16 +0800

    Integrate nlsc emap

--- frontend/src/components/OSM.vue
+++ frontend/src/components/OSM.vue
@@ -1,24 +1,52 @@
 <template>
-  <div id="map">
-  </div>
+  <div id="map"></div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import WMTS from "ol/source/WMTS";
+import WMTSTileGrid from "ol/tilegrid/WMTS";
+import { get as getProjection } from "ol/proj";
+import { getWidth, getTopLeft } from "ol/extent";
+
+import "ol/ol.css";
 
 @Component
 export default class OSM extends Vue {
-  mounted () {
+  mounted() {
+    const projection = getProjection("EPSG:3857");
+    const projectionExtent = projection.getExtent();
+    const resolutions = new Array(20);
+    const size = getWidth(projectionExtent) / 256;
+    const matrixIds = new Array(20);
+    for (let z = 0; z < 20; ++z) {
+      // generate resolutions and matrixIds arrays for this WMTS
+      resolutions[z] = size / Math.pow(2, z);
+      matrixIds[z] = z;
+    }
+
     new Map({
-      target: 'map',
+      target: "map",
       layers: [
         new TileLayer({
-          source: new XYZ({
-            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
+          source: new WMTS({
+            matrixSet: "EPSG:3857",
+            format: "image/png",
+            url: "https://wmts.nlsc.gov.tw/wmts",
+            layer: "EMAP",
+            tileGrid: new WMTSTileGrid({
+              origin: getTopLeft(projectionExtent),
+              resolutions: resolutions,
+              matrixIds: matrixIds
+            }),
+            style: "default",
+            wrapX: true,
+            attributions:
+              '<a href="https://maps.nlsc.gov.tw/" target="_blank">國土測繪圖資服務雲</a>'
+          }),
+          opacity: 0.5
         })
       ],
       view: new View({
