264c6414b52f4a51b4800bc6237c2e28fc62e6a9
stegben <stegben.benjamin@gmail.com>
2019-09-28 20:40:47 +0800

    add black config and auto format

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -3,29 +3,32 @@ from django.test import TestCase
 
 
 class CustomUserTest(TestCase):
-
     def test_create_user(self):
         User = get_user_model()
+        # fmt: off
         user = User.objects.create_user(
-            username = 'thehyyu',
-            email = 'backend@email.com',
-            password = 'backend123',
+            username="thehyyu",
+            email="backend@email.com",
+            password="backend123",
         )
-        self.assertEqual(user.username, 'thehyyu')
-        self.assertEqual(user.email, 'backend@email.com')
+        # fmt: on
+        self.assertEqual(user.username, "thehyyu")
+        self.assertEqual(user.email, "backend@email.com")
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
     def test_create_superuser(self):
         User = get_user_model()
+        # fmt: off
         admin_user = User.objects.create_superuser(
-            username = 'superadmin',
-            email = 'superadmin@email.com',
-            password = 'testpass123',
+            username="superadmin",
+            email="superadmin@email.com",
+            password="testpass123",
         )
-        self.assertEqual(admin_user.username, 'superadmin')
-        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        # fmt: on
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@email.com")
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
