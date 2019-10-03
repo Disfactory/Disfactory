c6c441890d0132bf5746a5de8c33a14bcb763e29
stegben <stegben.benjamin@gmail.com>
2019-10-03 12:46:44 +0800

    api models use contrib.gis.db

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,8 +1,8 @@
-from django.db import models
+from django.contrib.gis.db import models
 
 # TODO: Design and implement these models
 
-class Factory:
+class Factory(models.Model):
     """Factories that are potential to be illegal.
 
     This table should store information of a factory,
@@ -20,7 +20,7 @@ class Factory:
     pass
 
 
-class ReportRecord:
+class ReportRecord(models.Model):
     """Report records send by users.
 
     This table should contain the report record after calling these API:
@@ -39,7 +39,7 @@ class ReportRecord:
     pass
 
 
-class Image:
+class Image(models.Model):
     """Images of factories that are uploaded by user
 
     We store the actual image files on Imgur, so this table
