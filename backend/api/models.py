6fea8e427643d9938353296db21420c56fb0dabc
stegben <stegben.benjamin@gmail.com>
2019-10-09 19:34:59 +0800

    fix some coding style issue in api/models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,22 +7,10 @@ from django.contrib.postgres.fields import JSONField
 
 
 class Factory(models.Model):
-    """Factories that are potential to be illegal.
-            Containing:
-           - id :  Safety concern (primary key)
-           - fact_name
-           - lat
-           - lng
-           - landcode
-           - fact_type
-           - status :  The status of the processes
-           - status_time :  The last time of updating the status
-           - others :  Attachment of the description
-           - creat_at
-       """
+    """Factories that are potential to be illegal."""
 
     # List of fact_type & status
-    fact_type_list = [
+    factory_type_list = [
         ('1','金屬'),
         ('2-1','沖床、銑床、車床、鏜孔'),
         ('2-2', '焊接、鑄造、熱處理'),
@@ -55,7 +43,7 @@ class Factory(models.Model):
     landcode = models.CharField (max_length=50, blank=True, null=True)
 
     name = models.CharField(max_length=50, blank=True, null=True)
-    factory_type = models.CharField(max_length=3, choices=fact_type_list, default='9')
+    factory_type = models.CharField(max_length=3, choices=factory_type_list, default="9")
     status = models.CharField(max_length=1, choices=status_list)
     status_time = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -66,37 +54,24 @@ class Factory(models.Model):
 
 class ReportRecord(models.Model):
     """Report records send by users.
-        `ReportRecord` will be queried in advanced by admins from
-        Citizen of the Earth, Taiwan. They will filter the most recent
-        records out every a few weeks to catch the bad guys.
-        Containing:
-        - id : Foreign Key related to "Factory"
-        - user_ip
-        - action_type :  (e.g. PUT)
-        - action_body :  The request
-        - created_at
-        - contect :  Could be emall, phone number, nick name and so on
-       """
+
+    `ReportRecord` will be queried in advanced by admins from
+    Citizen of the Earth, Taiwan. They will filter the most recent
+    records out every a few weeks to catch the bad guys.
+    """
+
     id = models.AutoField(primary_key=True)
     factory = models.ForeignKey('Factory', on_delete=models.PROTECT)
     user_ip = models.GenericIPAddressField(default='192.168.0.1', blank=True, null=True)
-    action_type = models.CharField(max_length=10)
-    action_body = JSONField()
+    action_type = models.CharField(max_length=10)  # PUT, POST
+    action_body = JSONField()  # request body
     created_at = models.DateTimeField(auto_now_add=True)
     contact = models.CharField(max_length=64, blank=True, null=True)
     others = models.CharField(max_length=1024, blank=True)
 
 
 class Image(models.Model):
-    """Images of factories that are uploaded by user
-        We store the actual image files on Imgur, so this table
-        should contain :
-        - id :  Foreign Key related to "Factory"
-        - image_id
-        - image_path
-        - created_at :  Uploading time
-        - orig_time :  The time of taking the picture
-        """
+    """Images of factories that are uploaded by user."""
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
@@ -109,7 +84,9 @@ class Image(models.Model):
         blank=True,
         null=True,
     )
-    image_path = models.URLField(max_length=256)
+    image_path = models.URLField(max_length=256)  # get from Imgur
     created_at = models.DateTimeField(auto_now_add=True)
+    # the DB saving time
+
     orig_time = models.DateTimeField(blank=True, null=True)
-    pass
+    # the actual photo taken time
