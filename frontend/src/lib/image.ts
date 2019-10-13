24dfa8be246b80881d324606f7f2f64c76f63866
Yukai Huang <yukaihuangtw@gmail.com>
2019-10-13 16:42:28 +0800

    Transform argricultural land to transparent layer
    - otherwise transform to black

--- frontend/src/lib/image.ts
+++ frontend/src/lib/image.ts
@@ -45,3 +45,48 @@ export const toGrayScale = createImageProcessor(data => {
     data[i + 2] = avg // blue
   }
 })
+
+const argriculturalLandColors = [
+  [152, 230, 0],
+  [209, 255, 115],
+  [171, 220, 97],
+  [110, 221, 97],
+  [233, 255, 190],
+  [198, 230, 150],
+  [56, 204, 61],
+  [200, 205, 56],
+  [126, 237, 39],
+  [99, 192, 59],
+  [170, 192, 60],
+  [142, 169, 68],
+  [138, 255, 218],
+  [159, 177, 105],
+  [112, 134, 79],
+  [147, 203, 62],
+  [168, 168, 0],
+  [107, 144, 75],
+  [77, 101, 57]
+]
+
+const isArrayEqual = (a: Array<any>, b: Array<any>) => !a.some((v, i) => b[i] !== v)
+
+const isArgriculturalLand = (arr: Array<any>) => argriculturalLandColors.some(c => isArrayEqual(c, arr))
+
+export const flipArgriculturalLand = createImageProcessor(data => {
+  for (let i = 0; i < data.length; i += 4) {
+    const color = Array.from(data.slice(i, i + 3))
+
+    if (isArgriculturalLand(color)) {
+      // set to transparent dark
+      data[i] = 0
+      data[i + 1] = 0
+      data[i + 2] = 0
+      data[i + 3] = 0
+    } else {
+      // set to black
+      data[i] = 0
+      data[i + 1] = 0
+      data[i + 2] = 0
+    }
+  }
+})
