5f7f56fe08beb74501c2db4b6fa93f76658e559e
Pomin Wu <pomin5@gmail.com>
2019-10-15 16:47:20 +0800

    Fix coding style

--- backend/easymap.py
+++ backend/easymap.py
@@ -3,11 +3,13 @@
 import requests
 import re
 
+
 class WebRequestError(RuntimeError):
     def __init__(self, message, status_code, response_body):
         super().__init__(message)
         self.status_code = status_code
         self.response_body = response_body
+
 
 def get_session():
     easymap_url = "http://easymap.land.moi.gov.tw/P02/Index"
@@ -19,43 +21,39 @@ def get_session():
         raise RuntimeError("Failed getting session from easymap")
     return sess
 
+
 def get_point_city(sess, x, y):
     point_city_url = "http://easymap.land.moi.gov.tw/P02/Query_json_getPointCity"
-    data = { "wgs84x": x, "wgs84y": y }
+    data = {"wgs84x": x, "wgs84y": y}
     resp = sess.post(point_city_url, data=data)
     if resp.status_code != requests.codes.ok:
-        raise WebRequestError("Failed getting city code",
-                resp.status_code, resp.text)
+        raise WebRequestError("Failed getting city code", resp.status_code, resp.text)
     return resp.json()
+
 
 def get_token(sess):
     set_token_url = "http://easymap.land.moi.gov.tw/P02/pages/setToken.jsp"
     token_re = re.compile('<input type="hidden" name="(.*?)" value="(.*?)" />')
     resp = sess.post(set_token_url)
     if resp.status_code != requests.codes.ok:
-        raise WebRequestError("Failed getting token",
-                resp.status_code, resp.text)
-    token = dict([
-        (m.group(1), m.group(2))
-        for m in token_re.finditer(resp.text)
-        ])
+        raise WebRequestError("Failed getting token", resp.status_code, resp.text)
+    token = dict([(m.group(1), m.group(2)) for m in token_re.finditer(resp.text)])
     if not "token" in token:
-        raise WebRequestError("Failed parsing token",
-                resp.status_code, resp.text)
+        raise WebRequestError("Failed parsing token", resp.status_code, resp.text)
     return token
+
 
 def get_door_info(sess, x, y, city, token):
     get_door_info_url = "http://easymap.land.moi.gov.tw/P02/Door_json_getDoorInfoByXY"
-    data = { "city": city["cityCode"], "coordX": x, "coordY": y, **token }
+    data = {"city": city["cityCode"], "coordX": x, "coordY": y, **token}
     resp = sess.post(get_door_info_url, data=data)
     if resp.status_code != requests.codes.ok:
-        raise WebRequestError("Failed getting door info",
-                resp.status_code, resp.text)
+        raise WebRequestError("Failed getting door info", resp.status_code, resp.text)
     try:
         return resp.json()
     except:
-        raise WebRequestError("Failed parsing door info",
-                resp.status_code, resp.text)
+        raise WebRequestError("Failed parsing door info", resp.status_code, resp.text)
+
 
 def get_land_number(x, y):
     """
@@ -67,8 +65,10 @@ def get_land_number(x, y):
     land_number = get_door_info(sess, x=x, y=y, city=city, token=token)
     return land_number
 
+
 if __name__ == "__main__":
     import sys
+
     if len(sys.argv) != 3:
         print("Usage: easymap.py <wgs84x> <wgs84y>")
         sys.exit(-1)
