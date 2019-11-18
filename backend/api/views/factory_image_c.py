af4d36eabd0a6a7c68f4d3a6393dace27df670fb
stegben <stegben.benjamin@gmail.com>
2019-11-18 21:30:57 +0800

    implement and test post factory image

--- backend/api/views/factory_image_c.py
+++ backend/api/views/factory_image_c.py
@@ -1,19 +1,52 @@
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
+from django.db import transaction
 from rest_framework.decorators import api_view
 
-from .utils import _upload_image
+from ..models import Image, Factory, ReportRecord
+from ..serializers import ImageSerializer
+from .utils import (
+    _is_image,
+    _upload_image,
+    _get_image_original_date,
+    _get_client_ip,
+)
 
 
 @api_view(['POST'])
 def post_factory_image(request, factory_id):
-    # TODO
-    # f_image = request.FILE
-    # path = _upload_image(f_image)
-    # factory = Factory.objects.get(pk=factory_id)
-    # args = {
-    #     'uri': path,
-    #     'factory': factory,
-    # }
-    # img = Image.object.create(**args)
-    # token = img.token
-    # return Response(...)
-    pass
+    if not Factory.objects.filter(pk=factory_id).exists():
+        return HttpResponse(
+            f"Factory ID {factory_id} does not exist.",
+            status=400,
+        )
+    f_image = request.FILES['image']
+    if not _is_image(f_image):
+        return HttpResponse(
+            "The uploaded file cannot be parsed to Image",
+            status=400,
+        )
+
+    f_image.seek(0)
+    path = _upload_image(f_image, settings.IMGUR_CLIENT_ID)
+    f_image.seek(0)
+    image_original_date = _get_image_original_date(f_image)
+
+    client_ip = _get_client_ip(request)
+
+    with transaction.atomic():
+        factory = Factory.objects.only("id").get(pk=factory_id)
+        report_record = ReportRecord.objects.create(
+            factory=factory,
+            user_ip=client_ip,
+            action_type="POST_IMAGE",
+            action_body={},
+        )
+        img = Image.objects.create(
+            image_path=path,
+            orig_time=image_original_date,
+            factory=factory,
+            report_record=report_record,
+        )
+    img_serializer = ImageSerializer(img)
+    return JsonResponse(img_serializer.data, safe=False)
