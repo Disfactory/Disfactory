96807e9c9b4cfa091178ca3853cd842c061f12a3
stegben <stegben.benjamin@gmail.com>
2019-11-22 18:20:37 +0800

    implement factories_u

--- backend/api/views/factories_u.py
+++ backend/api/views/factories_u.py
@@ -1,36 +1,60 @@
+import json
+from datetime import datetime
+
 from rest_framework.decorators import api_view
 
-from django.http import HttpResponse
-from ..models import Factory
-
+from django.db import transaction
+from django.http import JsonResponse
+from django.conf import settings
 from django.contrib.gis.geos import Point
 
+from ..models import Factory, ReportRecord
+from ..serializers import FactorySerializer
+
+from .utils import _get_client_ip
+
+
 #@api_view(['PUT'])
-def update_factory_attribute(request):#, factory_id):
-    # GIT before coding
-    # git branch issue-name
-    # ...
-    # git pull
-    # git push origin issue-name
-    # factory = Factory.objects.get(pk=factory_id)
-    # factory.update(pk=factory_id)
+def update_factory_attribute(request, factory_id):
+    put_body = json.loads(request.body)
+    serializer = FactorySerializer(data=put_body, partial=True)
 
-    # 1. validate
-    # serialize
+    if not serializer.is_valid():
+        return JsonResponse(
+            serializer.errors,
+            status=400,
+        )
 
-    # 2. parse GET body
-    lat=request.GET['lat']
-    lng=request.GET['lng']
-    factory_id=request.GET['factory_id']
-#   return HttpResponse([lat,lng,factory_id])
+    updated_factory_fields = put_body.copy()
+    updated_factory_fields.pop("others", None)
+    updated_factory_fields.pop("contact", None)
 
-  # Revising to read GET body from urls.py /factories_update?factory_id=abc&lat=23.5&lng=120
- #  Factory.objects.filter(id=factory_id).update(**request['PUT'])
-    queryset=Factory.objects.filter(id=factory_id)
+    new_lng = put_body.get("lng")
+    new_lat = put_body.get("lat")
+    if (new_lat is not None) or (new_lng is not None):
+        factory = Factory.objects.only("lat", "lng").get(pk=factory_id)
+        new_lng = new_lng or factory.lng
+        new_lat = new_lat or factory.lat
+        new_point = Point(new_lng, new_lat, srid=4326)
+        new_point.transform(settings.POSTGIS_SRID)
+        updated_factory_fields["point"] = new_point
 
-    # 3. update database
-    queryset.update(lat=lat)
-    queryset.update(lng=lng)
-    return HttpResponse(['lat=',queryset[0].lat,' lng=',queryset[0].lng,' id=',queryset[0].id])
-#   queryset.update(point=Point(1,1))
-#   queryset.update(status_time='2019-11-20')
+    if "status" in put_body:
+        updated_factory_fields["status_time"] = datetime.now()
+
+    new_report_record_fields = {
+        "factory_id": factory_id,
+        "user_ip": _get_client_ip(request),
+        "action_type": "UPDATE",
+        "action_body": put_body,
+        "contact": put_body.get("contact"),
+        "others": put_body.get("others", ""),
+    }
+
+    with transaction.atomic():
+        Factory.objects.filter(pk=factory_id).update(**updated_factory_fields)
+        ReportRecord.objects.create(**new_report_record_fields)
+        factory = Factory.objects.get(pk=factory_id)
+
+    serializer = FactorySerializer(factory)
+    return JsonResponse(serializer.data, safe=False)
