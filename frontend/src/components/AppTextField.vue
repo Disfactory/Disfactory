dfef017be16d547787194f54e2090d870e3f04e0
yoyo930021 <yoyo930021@gmail.com>
2019-11-25 11:44:48 +0800

    Add base components

---
+++ frontend/src/components/AppTextField.vue
@@ -0,0 +1,50 @@
+<template>
+  <input
+    v-bind="$attrs"
+    v-model="inputValue"
+    :type="type"
+  >
+</template>
+
+<script>
+import { computed, createComponent } from '@vue/composition-api'
+export default createComponent({
+  name: 'AppTextField',
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  setup (props, context) {
+    const inputValue = computed({
+      get: () => props.value,
+      set: (value) => context.emit('input', value)
+    })
+
+    return {
+      inputValue
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+input {
+  border: 1px solid $second-color;
+  border-radius: 4px;
+  padding: 10px 15px;
+  outline: none;
+
+  font-size: $form-font-size;
+
+  &::placeholder {
+    color: rgb(186, 186, 186);
+  }
+}
+</style>
