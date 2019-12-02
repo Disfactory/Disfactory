c01861f81635cec51c2aedb663cfa1f417bc1787
Yukai Huang <yukaihuangtw@gmail.com>
2019-12-02 20:49:35 +0800

    Choose location center point

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -2,6 +2,8 @@
   <div class="map-container">
     <div ref="root" class="map" />
 
+    <div class="center-point" v-if="selectFactoryMode" />
+
     <div class="factory-button-group">
       <div class="create-factory-button" v-if="!selectFactoryMode">
         <app-button @click="toggleFactoryPage">我要新增違建工廠</app-button>
@@ -58,6 +60,8 @@ export default createComponent({
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/variables';
+
 .map-container {
   top: 47px;
   bottom: 0;
@@ -81,5 +85,21 @@ export default createComponent({
   .choose-location-button {
     transform: translateX(calc(50vw - 86.835px));
   }
+}
+
+.center-point {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  display: block;
+  background-color: $red-color;
+  border: solid 2px white;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+
+  transform: translate(calc(50vw - 12.5px), calc(50vw + 47px - 12.5px));
 }
 </style>
