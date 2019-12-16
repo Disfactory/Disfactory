d5f3439a18959668f69fe4cdacf303d0b5087276
Yukai Huang <yukaihuangtw@gmail.com>
2019-12-16 21:32:38 +0800

    Workaround iOS safari cache invalidation

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,5 +10,13 @@ module.exports = {
         }
       }
     }
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'development') {
+      config
+        .output
+        .filename('[name].[hash].js')
+        .end()
+    }
   }
 }
