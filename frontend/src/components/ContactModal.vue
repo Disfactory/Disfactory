ee17b5d7d87509193f8f0a9e8df43492c4e1596e
Yukai Huang <yukaihuangtw@gmail.com>
2019-12-22 16:03:00 +0800

    Style contact modal

--- frontend/src/components/ContactModal.vue
+++ frontend/src/components/ContactModal.vue
@@ -1,9 +1,22 @@
 <template>
-  <app-modal :open="open" :dismiss="dismiss">
-    <div class="page">
+  <div class="contact-modal-container">
+    <app-modal :open="open" :dismiss="dismiss">
+      <div class="page">
+        <img width="137" src="/images/cet-logo.png" alt="Citizen of the Earth Taiwan">
 
-    </div>
-  </app-modal>
+        <h3>
+          有任何舉報相關問題<br>
+          歡迎聯絡
+        </h3>
+
+        <p>
+          地球公民基金會<br>
+          TEL: 07-2156809<br>
+          Email: <a href="mailto:cet@cet-taiwan.org">cet@cet-taiwan.org</a><br>
+        </p>
+      </div>
+    </app-modal>
+  </div>
 </template>
 
 <script lang="ts">
@@ -27,11 +40,26 @@ export default createComponent({
 })
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import '@/styles/page';
+@import '@/styles/variables';
 
-.page-inner {
-  margin-bottom: 20px;
-  padding: 0 10px;
+.page h3 {
+  color: $primary-color;
+}
+
+.contact-modal-container .app-modal {
+  top: 47px;
+  max-height: calc(100% - 100px);
+  margin: 0 7px;
+  padding-top: 60px;
+
+  .page {
+    text-align: center;
+
+    h3 {
+      line-height: 1.5em;
+    }
+  }
 }
 </style>
