a44862b3426107ac7fbef1daae5a7d4508c85e8a
stegben <stegben.benjamin@gmail.com>
2020-01-08 20:28:38 +0800

    let upload image use async upload

--- backend/api/views/tests/test_image_c.py
+++ backend/api/views/tests/test_image_c.py
@@ -15,13 +15,15 @@ FAKE_IMGUR_PATH = "https://i.imgur.com/RxArJUc.png"
 
 class PostImageViewTestCase(TestCase):
 
-    @patch("api.views.image_c._upload_image", return_value=FAKE_IMGUR_PATH)
-    def test_image_with_exif_db_correct(self, patch_upload):
+    @patch("django_q.tasks.async_task")
+    def test_image_with_exif_db_correct(self, patch_async_tasks):
         cli = Client()
         test_time = datetime(2019, 11, 11, 11, 11, 11, tzinfo=timezone.utc)
         with freeze_time(test_time):
             with open(HERE / "20180311_132133.jpg", "rb") as f_img:
                 resp = cli.post("/api/images", {'image': f_img}, format='multipart')
+                f_img.seek(0)
+                image_buffer = f_img.read()
 
         self.assertEqual(resp.status_code, 200)
         resp_data = resp.json()
@@ -29,18 +31,25 @@ class PostImageViewTestCase(TestCase):
 
         img_id = resp_data['token']
         img = Image.objects.get(pk=img_id)
-        self.assertEqual(img.image_path, FAKE_IMGUR_PATH)
         self.assertEqual(img.created_at, test_time)
         self.assertEqual(img.orig_time, datetime(2018, 3, 11, 13, 21, 33, tzinfo=timezone.utc))
+        patch_async_tasks.assert_called_once_with(
+            'api.tasks.upload_image',
+            image_buffer,
+            settings.IMGUR_CLIENT_ID,
+            img.id,
+        )
 
     @patch("api.views.image_c._get_image_original_date", return_value=None)
-    @patch("api.views.image_c._upload_image", return_value=FAKE_IMGUR_PATH)
-    def test_image_without_exif_db_correct(self, patch_upload, _):
+    @patch("django_q.tasks.async_task")
+    def test_image_without_exif_db_correct(self, patch_async_tasks, _):
         cli = Client()
         test_time = datetime(2019, 11, 11, 11, 11, 11, tzinfo=timezone.utc)
         with freeze_time(test_time):
             with open(HERE / "20180311_132133.jpg", "rb") as f_img:
                 resp = cli.post("/api/images", {'image': f_img}, format='multipart')
+                f_img.seek(0)
+                image_buffer = f_img.read()
 
         resp_data = resp.json()
         self.assertEqual(resp.status_code, 200)
@@ -48,12 +57,17 @@ class PostImageViewTestCase(TestCase):
 
         img_id = resp_data['token']
         img = Image.objects.get(pk=img_id)
-        self.assertEqual(img.image_path, FAKE_IMGUR_PATH)
         self.assertEqual(img.created_at, test_time)
         self.assertIsNone(img.orig_time)
+        patch_async_tasks.assert_called_once_with(
+            'api.tasks.upload_image',
+            image_buffer,
+            settings.IMGUR_CLIENT_ID,
+            img.id,
+        )
 
-    @patch("api.views.image_c._upload_image", return_value=FAKE_IMGUR_PATH)
-    def test_return_400_if_not_image(self, patch_upload):
+    @patch("django_q.tasks.async_task")
+    def test_return_400_if_not_image(self, _):
         cli = Client()
         with open(HERE / "test_image_c.py", "rb") as f_img:
             resp = cli.post("/api/images", {'image': f_img}, format='multipart')
