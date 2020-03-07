25a8b0115a66e98127a8dda7560c9971c93ba469
stegben <stegben.benjamin@gmail.com>
2020-03-07 16:54:28 +0800

    add more log when uploading image

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -29,11 +29,26 @@ def update_landcode(factory_id):
 
 
 def upload_image(image_path, client_id, image_id):
-    with open(image_path, 'rb') as f:
-        image_buffer = f.read()
+    LOGGER.info(f'Upload {image_id}: {image_path} with {client_id}')
     try:
+        with open(image_path, 'rb') as f:
+            image_buffer = f.read()
         path = _upload_image_to_imgur(image_buffer, client_id)
-    except Exception:
-        LOGGER.warning(f"Upload {image_path} to Imgur failed")
-    Image.objects.filter(pk=image_id).update(image_path=path)
-    os.remove(image_path)
+    except Exception as e:
+        LOGGER.error(f"Upload {image_path} to Imgur with client ID {client_id} failed {e}")
+
+    try:
+        Image.objects.filter(pk=image_id).update(image_path=path)
+    except Exception as e:
+        LOGGER.error(f"""
+            Upload success and get imgur url {path},
+            but other error happened when update image {image_id}
+        """)
+
+    try:
+        os.remove(image_path)
+    except Exception as e:
+        LOGGER.warning(f"""
+            {image_id} upload and write to DB success,
+            but other error happened when removing tempfile {image_path}.
+        """)
