2280797b4a96fc4fb986c158b1e4b358d65b8059
stegben <stegben.benjamin@gmail.com>
2020-04-11 18:41:32 +0800

    add towninfo test

--- backend/towninfo/__init__.py
+++ backend/towninfo/__init__.py
@@ -2,7 +2,7 @@ import pathlib
 import xml.etree.ElementTree as ET
 
 
-HERE = pathlib.Path(__file__).home()
+HERE = pathlib.Path(__file__).parent
 code2name = {}
 name2code = {}
 
@@ -11,9 +11,14 @@ for xml_path in HERE.glob('*.xml'):
         tree = ET.fromstring(f.read())
         if tree:
             for child in tree.getchildren():
-                code = child[1].text
-                name = child[2].text
+                code = child.find('towncode').text
+                name = child.find('townname').text
                 code2name[code] = name
                 name2code[name] = code
 
+                code01_node = child.find('towncode01')
+                if code01_node is not None:
+                    code01 = code01_node.text
+                    code2name[code01] = name
+                    name2code[name] = code01
 
