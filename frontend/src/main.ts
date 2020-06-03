b30314cebaca1bdd38a6caedb5dcba20d6ce06d8
Yukai Huang <yukaihuangtw@gmail.com>
2020-06-03 19:55:40 +0800

    Setup vuetify

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueCompositionApi from '@vue/composition-api'
 import './registerServiceWorker'
 import VueGtag from 'vue-gtag'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionApi)
@@ -13,5 +14,6 @@ Vue.use(VueGtag, {
 })
 
 new Vue({
+  vuetify,
   render: (h) => h(App)
 }).$mount('#app')
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,30 +1,25 @@
 module.exports = {
-  lintOnSave: false,
-  devServer: {
-    proxy: {
-      '/server': {
-        target: 'https://staging.disfactory.tw',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/server': ''
+  "lintOnSave": false,
+  "devServer": {
+    "proxy": {
+      "/server": {
+        "target": "https://staging.disfactory.tw",
+        "changeOrigin": true,
+        "pathRewrite": {
+          "^/server": ""
         }
       }
     }
   },
-  chainWebpack: config => {
-    if (process.env.NODE_ENV === 'development') {
-      config
-        .output
-        .filename('[name].[hash].js')
-        .end()
+  "pwa": {
+    "name": "農地違章工廠",
+    "themeColor": "#2196f3",
+    "workboxOptions": {
+      "skipWaiting": true,
+      "clientsClaim": true
     }
   },
-  pwa: {
-    name: '農地違章工廠',
-    themeColor: '#2196f3',
-    workboxOptions: {
-      skipWaiting: true,
-      clientsClaim: true
-    }
-  }
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
