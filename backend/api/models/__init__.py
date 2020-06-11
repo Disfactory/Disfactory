9089fe76c10b4caebe8b9d740e17cfc9d7809afe
stegben <stegben.benjamin@gmail.com>
2020-06-11 21:34:07 +0800

    add review related tables

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,3 +1,6 @@
 from .factory import Factory
 from .report_record import ReportRecord
 from .image import Image
+from .document import Document, FollowUp
+from .review import Review
+from .gov_agency import GovAgency
