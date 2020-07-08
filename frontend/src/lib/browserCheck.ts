68dded6425b3c0e227d402b2cd7c5807aaecc5b5
Yukai Huang <yukaihuangtw@gmail.com>
2020-07-08 21:40:17 +0800

    Add ios version alert modal

--- frontend/src/lib/browserCheck.ts
+++ frontend/src/lib/browserCheck.ts
@@ -2,3 +2,17 @@
 export const isiOS = !!(navigator.userAgent.match(/(iPod|iPhone|iPad)/) && navigator.userAgent.match(/AppleWebKit/))
 
 export const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+
+export function iOSversion () {
+  if (/iP(hone|od|ad)/.test(navigator.platform)) {
+    const v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+    if (v) {
+      return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || '0', 10)].join('.')
+    }
+  }
+}
+
+export function isNotSupportedIOS () {
+  let version
+  return !!isiOS && !!(version = iOSversion()) && !!version.match(/^13\.(1|2|3)/)
+}
