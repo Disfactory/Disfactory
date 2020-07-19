3dbf425000934a23c593ae36e35311753ae94425
Yukai Huang <yukaihuangtw@gmail.com>
2020-07-19 23:28:11 +0800

    Remove necessary group model

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -31,7 +31,6 @@
         dense
       >
         <v-list-item-group
-          v-model="group"
           active-class="deep-purple--text text--accent-4"
         >
           <v-list-item @click="modalActions.openTutorialModal">
