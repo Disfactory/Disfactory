0896449c5a0d6fafc47c78f02d7a5f7427a6c6e6
LittleWhiteYA <f46284628@gmail.com>
2021-01-06 20:54:57 +0800

    feat: update factory.cet_review_status when generating documents

--- backend/api/admin/actions/generate_docs.py
+++ backend/api/admin/actions/generate_docs.py
@@ -1,7 +1,7 @@
 import datetime
 from django.db.models import Max
 
-from api.models import Document
+from api.models import Document, Factory
 from api.utils import set_function_attributes
 
 
@@ -31,14 +31,21 @@ class GenerateDocsMixin:
         if not previous_code or previous_code < taiwan_year * (10 ** 4):
             previous_code = taiwan_year * (10 ** 4)
 
-        docs = [
-            Document(
-                factory_id=factory.id,
-                creator_id=user.id,
-                code=code,
-                cet_staff=choose_cet_staff(factory.townname),
+        docs = []
+        factories = []
+
+        for code, factory in enumerate(queryset, start=previous_code + 1):
+            docs.append(
+                Document(
+                    factory_id=factory.id,
+                    creator_id=user.id,
+                    code=code,
+                    cet_staff=choose_cet_staff(factory.townname),
+                )
             )
-            for code, factory in enumerate(queryset, start=previous_code + 1)
-        ]
+
+            factory.cet_review_status = "X"
+            factories.append(factory)
 
         Document.objects.bulk_create(docs)
+        Factory.objects.bulk_update(factories, ["cet_review_status"])
