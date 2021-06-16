ce8b526b897a74fd903e001cf6b9c05cc93e2143
Swind <swind@cloudmosa.com>
2021-06-16 18:58:00 +0800

    Update easymap url and add testcases for easymap

--- backend/easymap.py
+++ backend/easymap.py
@@ -5,6 +5,7 @@ import re
 
 import towninfo
 
+DEFAULT_TIMEOUT = 5 # 5 seconds
 
 class WebRequestError(RuntimeError):
     def __init__(self, message, status_code, response_body):
@@ -13,21 +14,21 @@ class WebRequestError(RuntimeError):
         self.response_body = response_body
 
 
-def get_session():
-    easymap_url = "http://easymap.land.moi.gov.tw/P02/Index"
+def get_session(timeout=DEFAULT_TIMEOUT):
+    easymap_url = "https://easymap.land.moi.gov.tw/Index"
     sess = requests.Session()
     # XXX don't need this?
     # sess.headers.update({"User-Agent": "Mozilla/5.0"})
-    resp = sess.get(easymap_url)
+    resp = sess.get(easymap_url, timeout=timeout)
     if "JSESSIONID" not in sess.cookies:
         raise WebRequestError("Failed getting session from easymap", resp.status_code, resp.text)
     return sess
 
 
-def get_point_city(sess, x, y):
-    point_city_url = "http://easymap.land.moi.gov.tw/P02/Query_json_getPointCity"
+def get_point_city(sess, x, y, timeout=DEFAULT_TIMEOUT):
+    point_city_url = "https://easymap.land.moi.gov.tw/Query_json_getPointCity"
     data = {"wgs84x": x, "wgs84y": y}
-    resp = sess.post(point_city_url, data=data)
+    resp = sess.post(point_city_url, data=data, timeout=timeout)
     if resp.status_code != requests.codes.ok:
         raise WebRequestError("Failed getting city code", resp.status_code, resp.text)
     try:
@@ -36,10 +37,10 @@ def get_point_city(sess, x, y):
         raise WebRequestError("Failed parsing city code", resp.status_code, resp.text)
 
 
-def get_token(sess):
-    set_token_url = "http://easymap.land.moi.gov.tw/P02/pages/setToken.jsp"
+def get_token(sess, timeout=DEFAULT_TIMEOUT):
+    set_token_url = "https://easymap.land.moi.gov.tw/pages/setToken.jsp"
     token_re = re.compile('<input type="hidden" name="(.*?)" value="(.*?)" />')
-    resp = sess.post(set_token_url)
+    resp = sess.post(set_token_url, timeout=timeout)
     if resp.status_code != requests.codes.ok:
         raise WebRequestError("Failed getting token", resp.status_code, resp.text)
     token = dict([(m.group(1), m.group(2)) for m in token_re.finditer(resp.text)])
@@ -48,10 +49,10 @@ def get_token(sess):
     return token
 
 
-def get_door_info(sess, x, y, cityCode, token):
-    get_door_info_url = "http://easymap.land.moi.gov.tw/P02/Door_json_getDoorInfoByXY"
+def get_door_info(sess, x, y, cityCode, token, timeout=DEFAULT_TIMEOUT):
+    get_door_info_url = "https://easymap.land.moi.gov.tw/Door_json_getDoorInfoByXY"
     data = {"city": cityCode, "coordX": x, "coordY": y, **token}
-    resp = sess.post(get_door_info_url, data=data)
+    resp = sess.post(get_door_info_url, data=data, timeout=timeout)
     if resp.status_code != requests.codes.ok:
         raise WebRequestError("Failed getting door info", resp.status_code, resp.text)
     try:
@@ -60,16 +61,16 @@ def get_door_info(sess, x, y, cityCode, token):
         raise WebRequestError("Failed parsing door info", resp.status_code, resp.text)
 
 
-def get_land_number(x, y):
+def get_land_number(x, y, timeout=DEFAULT_TIMEOUT):
     """
     Get land number by WGS84 coordinates.
 
     since the easymap API doesn't provide townname, we then insert a townname field by looking up in xml files in ./towncode downloaded from https://api.nlsc.gov.tw/other/ListTown1/{A-Z}
     """
-    sess = get_session()
-    cityCode = get_point_city(sess, x=x, y=y)
-    token = get_token(sess)
-    land_number = get_door_info(sess, x=x, y=y, cityCode=cityCode, token=token)
+    sess = get_session(timeout=timeout)
+    cityCode = get_point_city(sess, x=x, y=y, timeout=timeout)
+    token = get_token(sess,timeout=timeout)
+    land_number = get_door_info(sess, x=x, y=y, cityCode=cityCode, token=token, timeout=timeout)
     sess.close()
     land_number["townname"] = towninfo.code2name.get(land_number["towncode"], "")
     return land_number
