2b6c7d4cbb9062d870b1580f2be0bc87ee20f831
Swind <swind@cloudmosa.com>
2021-10-20 18:18:30 +0800

    fix migration conflicts

--- backend/api/migrations/0038_followup_for_user.py
+++ backend/api/migrations/0038_followup_for_user.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0036_auto_20210825_2101'),
+        ('api', '0037_rename_display_status'),
     ]
 
     operations = [
