ee3038e70b3c4ad52011cc923cb9a863d6dc800d
Swind <swind@cloudmosa.com>
2022-03-10 01:03:22 +0800

    easymap 的 testcase 不需要透過 proxy 取資料

--- backend/api/views/tests/test_easymap.py
+++ backend/api/views/tests/test_easymap.py
@@ -3,7 +3,7 @@ import easymap
 def test_get_land_number():
     # 120.1074406, 23.2353021
     # 臺南市北門區 溪底寮段三寮灣小段 (5404) 1681地號
-    result = easymap.get_land_number(120.1074406, 23.2353021)
+    result = easymap.get_land_number(120.1074406, 23.2353021, proxies=None)
     assert result["towncode"] == "D24"
     assert result["townname"] == "臺南市北門區"
     assert result["sectno"] == "5404"
