0f788a2c942fe515ff61f2393f3b30db77fa9c65
Yukai Huang <yukaihuangtw@gmail.com>
2025-06-21 17:47:32 +0800

    Add static file URL routing to urlpatterns

--- backend/gis_project/urls.py
+++ backend/gis_project/urls.py
@@ -42,4 +42,4 @@ urlpatterns = [
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("", lambda _: redirect("https://dev.disfactory.tw/")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
