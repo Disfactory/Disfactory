437d007212220a95d0df61fdbcfd5941d5326d1c
Yukai Huang <yukaihuangtw@gmail.com>
2025-06-21 15:11:19 +0800

    Update Dockerfile and scripts for improved static file handling and migration process

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -6,4 +6,4 @@ ROOT_DIR="$SCRIPT_DIR/.."
 cd $ROOT_DIR
 
 echo "Waiting postgreSQL ${DISFACTORY_BACKEND_DEFAULT_DB_HOST}:${DISFACTORY_BACKEND_DEFAULT_DB_PORT}"
-$ROOT_DIR/scripts/wait-for-it.sh ${DISFACTORY_BACKEND_DEFAULT_DB_HOST}:${DISFACTORY_BACKEND_DEFAULT_DB_PORT} -- bash -c "python manage.py migrate;python manage.py runserver 0.0.0.0:8000"
+$ROOT_DIR/scripts/wait-for-it.sh ${DISFACTORY_BACKEND_DEFAULT_DB_HOST}:${DISFACTORY_BACKEND_DEFAULT_DB_PORT} -- bash -c "python manage.py collectstatic --noinput;python manage.py migrate;python manage.py runserver 0.0.0.0:8000"
